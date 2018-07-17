32d6dcad5e5f90f8e33bf0c1a5f466730b3501db
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-07-17 11:57:53 -0500

    Add files via upload

--- talleres/taller01/Punto.java
+++ talleres/taller01/Punto.java
@@ -1,30 +1,39 @@
-public class Punto {
-    private double x, y;
 
-    public Punto() {
-
+/**
+ * Write a description of class Punto here.
+ * 
+ * @author (Juan Sebastián Pérez Salazar) 
+ * @version (a version number or a date)
+ */
+public class Punto
+{
+    private double x, y; 
+    
+     public Punto(double x, double y) {
+         this.x = x;
+         this.y = y; 
     }
 
     public double x() {
-
+        return this.x; 
     }
 
     public double y() {
-
+        return this.y; 
     }
 
     // http://mathworld.wolfram.com/PolarCoordinates.html
     public double radioPolar() {
-
+        return Math.sqrt(Math.pow(x,2)+Math.pow(y,2)); 
     }
 
     // http://mathworld.wolfram.com/PolarCoordinates.html
     public double anguloPolar() {
-
+        return Math.atan2(y,x);
     }
 
     // http://mathworld.wolfram.com/Distance.html
     public double distanciaEuclidiana(Punto otro) {
-        
+        return Math.sqrt(Math.pow((x-otro.x()),2)+Math.pow((y-otro.y()),2));
     }
 }
