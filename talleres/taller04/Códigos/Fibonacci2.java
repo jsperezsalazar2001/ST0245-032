68f28ff72d6c9adcdbaaa4ff3221fd1f5b983ba4
yaguzmang <41337901+yaguzmang@users.noreply.github.com>
2018-08-16 11:05:48 -0500

    Add files via upload

---
+++ talleres/taller04/Códigos/Fibonacci2.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.TimeUnit;
+/**
+ * La clase Fibonacci tiene la intención de resolver el punto 3 del taller número 4.
+ *
+ * @author Yhoan Alejandro Guzmán, Sebastián Pérez
+ * @version 1
+ */
+
+public class Fibonacci2 {
+
+    /**
+     * @param n es el n-ésimo termino de la sucesión
+     * de fibonacci, que se quiere calcular
+     * Este método hace lo cálculos necesarios para obtener este n-ésimo término
+     */
+    public  int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        }catch (Exception e){
+            System.out.println("Error: "+e);
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    /**
+     * Este método es el main y sirve para ejecutar y probar el método fibonacci
+     */
+    public static void main(String[]args){
+        Fibonacci2 prueba = new Fibonacci2();
+        for(int i = 0; i < 15; i++ ){
+            long start = System.currentTimeMillis();
+            prueba.fibonacci(i);
+            long fin = System.currentTimeMillis();
+            System.out.println(("Tiempo para n = "+i)+" => "+(fin-start));
+        }
+    }
+}
