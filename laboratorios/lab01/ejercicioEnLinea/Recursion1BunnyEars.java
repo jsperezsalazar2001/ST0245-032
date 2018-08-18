64d0282b18c173783abe5c6d8ba6679059bbdfdf
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:01:22 -0500

    Update Recursion1BunnyEars.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion1BunnyEars.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1BunnyEars.java
@@ -5,6 +5,12 @@
  */
 public class Recursion1BunnyEars {
     
+    /**
+     * Method that solves the following statement of CodingBat - Recursion1:
+       "We have a number of bunnies and each bunny has two big floppy ears. 
+        We want to compute the total number of ears across all the bunnies 
+        recursively (without loops or multiplication)."
+     */   
     public int bunnyEars(int bunnies) {
         if(bunnies==0) return bunnies; 
         return 2+bunnyEars(bunnies-1);
