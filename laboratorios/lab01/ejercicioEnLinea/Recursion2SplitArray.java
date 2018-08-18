5fd55f04c26ea705ff47150f8e92d59dd13b7f46
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:12:31 -0500

    Update Recursion2SplitArray.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion2SplitArray.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2SplitArray.java
@@ -5,6 +5,14 @@
  */
 public class Recursion2SplitArray {
     
+    /**
+    * Method that solves the following statement of CodingBat - Recursion2:
+     "Given an array of ints, is it possible to divide the ints into two groups, 
+     so that the sums of the two groups are the same. Every int must be in one 
+     group or the other. Write a recursive helper method that takes whatever 
+     arguments you like, and make the initial call to your recursive helper from 
+     splitArray(). (No loops needed.)"
+    */
     public boolean splitArray(int[] nums) {
         int cont1=0;
         int cont2=0;
