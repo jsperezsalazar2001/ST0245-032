9b1c5b82b7adea783c0fe386f0f493c75ef60887
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:04:23 -0500

    Update Recursion1SumDigits.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion1SumDigits.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1SumDigits.java
@@ -5,6 +5,12 @@
  */
 public class Recursion1SumDigits {
     
+    /**
+     * Method that solves the following statement of CodingBat - Recursion1:
+       "Given a non-negative int n, return the sum of its digits recursively (no loops). 
+       Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 
+       removes the rightmost digit (126 / 10 is 12)."
+     */
     public int sumDigits(int n) {
         if(n==0)return n;
         return (n%10)+sumDigits(n/10);
