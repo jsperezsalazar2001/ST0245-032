0d1c5d109052818327199f5943e0eff922171bd7
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:05:29 -0500

    Update Recursion1Triangle.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion1Triangle.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1Triangle.java
@@ -5,6 +5,13 @@
  */
 public class Recursion1Triangle {
     
+    /**
+     * Method that solves the following statement of CodingBat - Recursion1:
+       "We have triangle made of blocks. The topmost row has 1 block, the next 
+        row down has 2 blocks, the next row has 3 blocks, and so on. Compute 
+        recursively (no loops or multiplication) the total number of blocks in 
+        such a triangle with the given number of rows."
+     */
     public int triangle(int rows) {
         if(rows==0) return 0;
         return rows+triangle(rows-1);
