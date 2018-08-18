8b90780a537f4ae08fc7a20820d75b79eba9f7a5
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:10:40 -0500

    Update Recursion2GroupSum6.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion2GroupSum6.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2GroupSum6.java
@@ -4,8 +4,8 @@
  * @author Yhoan Alejandro Guzman
  */
 public class Recursion2GroupSum6 {
-    /*
-    *Método que resuelve el siguiente enunciado de CodingBat - Recursion2:
+    /**
+    * Method that solves the following statement of CodingBat - Recursion2:
     "Given an array of ints, is it possible to choose a group of some of the ints, 
     beginning at the start index, such that the group sums to the given target? However, 
     with the additional constraint that all 6's must be chosen. (No loops needed.)"
