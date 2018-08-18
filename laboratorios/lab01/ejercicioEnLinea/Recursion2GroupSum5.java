9796bec17a987e8cd7a0e176cd0748449fe6e89d
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:09:33 -0500

    Update Recursion2GroupSum5.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion2GroupSum5.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2GroupSum5.java
@@ -5,6 +5,13 @@
  */
 public class Recursion2GroupSum5 {
     
+    /**
+    * Method that solves the following statement of CodingBat - Recursion2:
+      "Given an array of ints, is it possible to choose a group of some of the ints, 
+      such that the group sums to the given target with these additional constraints: 
+      all multiples of 5 in the array must be included in the group. If the value immediately 
+      following a multiple of 5 is 1, it must not be chosen. (No loops needed.)"
+    */ 
     public boolean groupSum5(int start, int[] nums, int target) {   
         if(start >= nums.length) return target == 0;
         if(nums[start]%5==0){
