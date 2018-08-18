49ef4d1aab2856d51eeebc1c2e4a5f58dd56e1e0
jsperezsalazar2001 <jsperezsalazar2001@gmail.com>
2018-08-18 13:03:25 -0500

    Update Recursion1Factorial.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion1Factorial.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1Factorial.java
@@ -5,6 +5,11 @@
  */
 public class Recursion1Factorial {
     
+    /**
+     * Method that solves the following statement of CodingBat - Recursion1:
+       "Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. 
+        Compute the result recursively (without loops)."
+     */
     public int factorial(int n) {
         if(n<=0) return 1;
         return n*factorial(n-1);
