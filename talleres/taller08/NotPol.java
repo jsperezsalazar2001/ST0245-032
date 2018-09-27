f8a889072d4646a35defab952bed47f016bf79a2
yaguzmang <41337901+yaguzmang@users.noreply.github.com>
2018-09-27 00:00:13 -0500

    Add files via upload

---
+++ talleres/taller08/NotPol.java
@@ -0,0 +1,67 @@
+import java.util.Stack;
+public class NotPol
+{
+    public static int notPol  (String expression){
+        String[] characters= expression.split(" ");
+        Stack<String> stack= new Stack<String>();
+        String aux;
+        int value=0;
+        for(int i=characters.length - 1; i >= 0; i--){
+            stack.push(characters[i]);
+        }
+        int [] nums = new int[characters.length - 1];
+        int index = 0;
+        int current = 0;
+        while(!(stack.isEmpty())){
+            aux = stack.pop();
+            if(isInteger(aux)){
+               nums[index] = Integer.parseInt(aux);
+               index++;
+            }else{
+                if(aux.equals("+")){
+                    value = nums[current];
+                    current++;
+                    value += nums[current];
+                    current++;
+                }
+                if(aux.equals("-")){
+                    value = nums[current];
+                    current++;
+                    value -= nums[current];
+                    current++;
+                }
+                if(aux.equals("*")){
+                    value = nums[current];
+                    current++;
+                    value *= nums[current];
+                    current++;
+                }
+                if(aux.equals("/")){
+                    value = nums[current];
+                    current++;
+                    value /= nums[current];
+                    current++;
+                }
+                if(aux.equals("%")){
+                    value = nums[current];
+                    current++;
+                    value %= nums[current];
+                    current++;
+                }
+            }
+        }
+        return value;
+    }
+
+    public static boolean isInteger(String s) {
+        try { 
+            Integer.parseInt(s); 
+        } catch(NumberFormatException e) { 
+            return false; 
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+
+}
